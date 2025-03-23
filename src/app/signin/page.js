72324198ac5799e06dd873bcd17b53b4133aa336
tejeswarnaidu9 <tejeswarnaidu9@gmail.com>
2025-03-23 10:26:08 +0000

    router import

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { auth, googleProvider , githubProvider } from "@/firebaseConfig";
+import { useRouter } from "next/navigation";
+
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
